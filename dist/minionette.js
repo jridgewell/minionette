ce374e153ec3a25b8cf47e370bc72b32c777bcaf
Justin Ridgewell <jridgewell@cloudspace.com>
2013-07-18 17:03:00 -0400

    Update distribution

--- dist/minionette.js
+++ dist/minionette.js
@@ -1,22 +1,13 @@
-// Backbone.Minionette
-// -------------------
-// v0.2.0
-//
-// Copyright (c)2013 Justin Ridgewell
-// Distributed under MIT license
-//
-// https://github.com/jridgewell/minionette
+var Minionette = (function(global, _, $, Backbone) {
+    'use strict';
 
-var Minionette = (function(global, _, $, Backbone){
-  "use strict";
+    // Define and export the Minionette namespace
+    var Minionette = {};
+    Backbone.Minionette = Minionette;
 
-  // Define and export the Minionette namespace
-  var Minionette = {};
-  Backbone.Minionette = Minionette;
-
-// Force jQuery to emit remove events
+    // Force jQuery to emit remove events.
 // Our views bind to it to see if they
-// have been removed and should be closed
+// have been removed and should be closed.
 $.event.special.remove = {
     remove: function(e) {
         if (e.handler) { e.handler(); }
@@ -24,55 +15,55 @@ $.event.special.remove = {
 };
 
 
-Minionette.View = Backbone.View.extend({
+    Minionette.View = Backbone.View.extend({
     constructor: function() {
         Backbone.View.apply(this, arguments);
 
-        // Done so we don't bindAll to standard methods
-        _.bindAll(this, '_jquery_remove', '_close');
+        // Done so we don't bindAll to standard methods.
+        _.bindAll(this, '_jqueryRemove', '_close');
 
-        // Keep track of our subviews
+        // Keep track of our subviews.
         this._subViews = {};
 
-        // Have the view listenTo the model and collection
+        // Have the view listenTo the model and collection.
         this._listenToEvents(this.model, this.modelEvents);
         this._listenToEvents(this.collection, this.collectionEvents);
         this.listenTo(this, 'close:before', this._close);
     },
 
-    // A default template that will clear this.$el
+    // A default template that will clear this.$el.
     // Override this in a subclass to something useful.
     template: function() { return ''; },
 
-    // When delegating events, bind this view to jQuery's special remove event
-    // Allows us to clean up the view, even if you remove this.$el with jQuery
+    // When delegating events, bind this view to jQuery's special remove event.
+    // Allows us to clean up the view, even if you remove this.$el with jQuery.
     // http://blog.alexmaccaw.com/jswebapps-memory-management
     delegateEvents: function(events) {
         Backbone.View.prototype.delegateEvents.apply(this, events);
-        this.$el.on('remove.delegateEvents' + this.cid, this._jquery_remove);
+        this.$el.on('remove.delegateEvents' + this.cid, this._jqueryRemove);
     },
 
-    // A useful remove method to that triggers events
-    // Notice we emit "close" events, not "remove"
+    // A useful remove method to that triggers events.
+    // Notice we emit "close" events, not "remove".
     close: function() {
         this.trigger('close:before');
         this.remove();
         this.trigger('close');
     },
 
-    // A called on close:before to remove all our subviews
+    // A called on close:before to remove all our subviews.
     _close: function() {
         _.each(this._subViews, function(view) { view.close(); });
     },
 
 
-    // Assign a subview to an element in my template
-    // selector is a dom selector to assign to
-    // view is the subview to assign the selector to
-    // replace is a boolean
-    //    False (default), set view's $el to the selector
-    //    True, replace the selector with view's $el
-    // Alternate syntax by passing in an object for selector
+    // Assign a subview to an element in my template.
+    // `selector` is a dom selector to assign to.
+    // `view` is the subview to assign the selector to.
+    // `replace` is a boolean.
+    //    `False` (default), set view's $el to the selector.
+    //    `True`, replace the selector with view's $el.
+    // Alternate syntax by passing in an object for selector.
     //    With "selector": subview
     //    Replace will be the second param in this case.
     assign : function (selector, view, replace) {
@@ -96,15 +87,15 @@ Minionette.View = Backbone.View.extend({
         }, this);
     },
 
-    // A proxy method to this.close()
+    // A proxy method to this.close().
     // This is bindAll-ed to the view instance.
-    // Done so that we don't need to bindAll to close()
-    _jquery_remove: function() {
+    // Done so that we don't need to bindAll to close().
+    _jqueryRemove: function() {
         this.close();
     },
 
     // Loop through the events given, and listen to
-    // entity's event
+    // entity's event.
     _listenToEvents: function(entity, events) {
         for (var event in events) {
             var method = events[event];
@@ -117,50 +108,50 @@ Minionette.View = Backbone.View.extend({
     }
 });
 
-Minionette.ModelView = Minionette.View.extend({
-    // The data that is sent into the template function
-    // Override this to provide custom data
+    Minionette.ModelView = Minionette.View.extend({
+    // The data that is sent into the template function.
+    // Override this to provide custom data.
     serializeData: function() {
         return this.model.attributes;
     },
 
-    // A useful default render method
+    // A useful default render method.
     render: function() {
         this.trigger('render:before');
 
-        // Detach all our subviews, so they don't need to be re-rendered
+        // Detach all our subviews, so they don't need to be re-rendered.
         _.each(this._subViews, function(view) { view.$el.detach(); });
 
         this.$el.html(this.template(this.serializeData()));
 
-        // Listen for render events to reattach subviews
+        // Listen for render events to reattach subviews.
         this.trigger('render');
         return this;
     }
 });
 
-Minionette.CollectionView = Minionette.View.extend({
+    Minionette.CollectionView = Minionette.View.extend({
 
-    // The View class to render the collection as
+    // The View class to render the collection as.
     ModelView: Backbone.View,
 
-    // Listen to the default events
+    // Listen to the default events.
     collectionEvents: {
         'add': 'addOne',
         'remove': 'removeOne',
         'reset': 'render'
     },
 
-    // A default useful render function
+    // A default useful render function.
     render: function() {
         this.trigger('render:before');
 
         this.$el.html(this.template(this.collection));
 
-        // Collect the ModelView class
+        // Collect the ModelView class.
         var ModelView = this._getModelView();
 
-        // Loop through all our models, and build their view
+        // Loop through all our models, and build their view.
         this.collection.each(function(model) {
             this._addModelView(model, ModelView);
         }, this);
@@ -169,18 +160,18 @@ Minionette.CollectionView = Minionette.View.extend({
         return this;
     },
 
-    // Add an individual model's view to this.$el
+    // Add an individual model's view to this.$el.
     addOne: function(model) {
         this.trigger('addOne:before');
 
-        // Collect the ModelView class
+        // Collect the ModelView class.
         var ModelView = this._getModelView();
         this._addModelView(model, ModelView);
 
         this.trigger('addOne');
     },
 
-    // Remove an individual model's view from this.$el
+    // Remove an individual model's view from this.$el.
     removeOne: function(model) {
         this.trigger('removeOne:before');
 
@@ -190,18 +181,18 @@ Minionette.CollectionView = Minionette.View.extend({
         this.trigger('removeOne');
     },
 
-    // Add an individual model's view to this.$el
+    // Add an individual model's view to this.$el.
     _addModelView: function(model, ModelView) {
         var modelView = new ModelView({model: model});
 
         // Add this view to our subviews, so we can close
-        // them later
+        // them later.
         this._subViews[modelView.cid] = modelView;
         this.$el.append(modelView.render().el);
     },
 
-    // Find the correct ModelView to use
-    // Prioritizes the one passed at initialization
+    // Find the correct ModelView to use.
+    // Prioritizes the one passed at initialization.
     _getModelView: function() {
         var ModelView = this.ModelView;
         if (this.options && this.options.ModelView) {
@@ -210,25 +201,25 @@ Minionette.CollectionView = Minionette.View.extend({
         return ModelView;
     },
 
-    // Remove an individual model's view from this.$el
+    // Remove an individual model's view from this.$el.
     _removeModelView: function(view) {
-        // Check that view exists first
+        // Check that view exists first.
         // Cuased be removeOne() trying to remove a model
         // that we haven't added yet.
         if (view) {
             view.close();
 
-            // Remove it from our subviews
+            // Remove it from our subviews.
             delete this._subViews[view.cid];
         }
     },
 
-    // Find the view associated with a model from our subviews
+    // Find the view associated with a model from our subviews.
     _findSubViewByModel: function(model) {
         return _.findWhere(this._subViews, {model: model});
     }
 });
 
 
-  return Minionette;
+    return Minionette;
 })(this, _, jQuery, Backbone);
