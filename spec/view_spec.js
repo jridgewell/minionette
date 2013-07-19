956eb5738f8d3f5c4d9972bf1d8348dd1c4bafee
Justin Ridgewell <justin@ridgewell.name>
2013-07-19 19:42:30 -0400

    Begin specing Minionette.View

---
+++ spec/view_spec.js
@@ -0,0 +1,8 @@
+define(function(require) {
+    var Minionette = require('minionette');
+    describe('Minionette.View', function() {
+        it('Exists', function() {
+            expect(Minionette.View).to.not.be.undefined;
+        });
+    });
+});
