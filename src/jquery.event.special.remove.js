019b8377222bc7f624dfaf96e919d1a32acd9e83
Justin Ridgewell <justin@ridgewell.name>
2013-07-19 00:04:35 -0400

    Use #remove insead of #close

--- src/jquery.event.special.remove.js
+++ src/jquery.event.special.remove.js
@@ -1,6 +1,6 @@
 // Force jQuery to emit remove events.
 // Our views bind to it to see if they
-// have been removed and should be closed.
+// have been removed and should be cleaned.
 $.event.special.remove = {
     remove: function(e) {
         if (e.handler) { e.handler(); }
