f1be40c8da1b57a7af6f685036424cdcd3c4b5d1
Justin Ridgewell <justin@ridgewell.name>
2013-07-21 01:08:31 -0400

    Allow Chai.should syntax

--- spec/common.js
+++ spec/common.js
@@ -2,12 +2,13 @@ define(function(require) {
     window.$ = window.jQuery = require('jquery');
     window.chai              = require('chai');
     window.sinon             = require('sinon');
-    window.expect            = chai.expect;
-    window.assert            = chai.assert;
     window.sinonChai         = require('sinon-chai');
     window.jqueryChai        = require('chai-jquery');
     window.Minionette        = require('minionette');
 
+    window.expect = chai.expect;
+    window.assert = chai.assert;
+    chai.should();
     chai.use(sinonChai);
     chai.use(jqueryChai);
 
