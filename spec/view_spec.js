00d8ae4092207987b33415d8a3c3b9a95e21904c
Justin Ridgewell <justin@ridgewell.name>
2013-07-21 01:09:02 -0400

    Expect a call to super

--- spec/view_spec.js
+++ spec/view_spec.js
@@ -3,5 +3,13 @@ define(function(require) {
         it('Exists', function() {
             expect(Minionette.View).to.not.be.undefined;
         });
+
+        it("calls Backbone.View's constructor", function() {
+            var spy = Backbone.View = this.sinon.spy();
+
+            new Minionette.View;
+
+            spy.should.have.been.called;
+        })
     });
 });
