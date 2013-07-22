c0717e05571d5ee6c89abb00232be65a9c359541
Justin Ridgewell <jridgewell@cloudspace.com>
2013-07-22 17:49:20 -0400

    Spec the jQuery remove polyfill

---
+++ spec/jquery_spec.js
@@ -0,0 +1,23 @@
+define(function(require) {
+    describe('jQuery Remove Polyfill', function() {
+        beforeEach(function() {
+            this.element = $('<div />');
+        });
+        beforeEach(function() {
+            this.spy = this.sinon.spy();
+            this.element.on('remove', this.spy);
+        });
+
+        it("triggers when the event is unbound", function() {
+            this.element.off('remove');
+
+            expect(this.spy).to.have.been.called;
+        });
+
+        it("triggers when element is removed", function() {
+            this.element.remove();
+
+            expect(this.spy).to.have.been.called;
+        });
+    });
+});
