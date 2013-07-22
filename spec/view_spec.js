6ac9eafc63899070b90fc63237170c18139307f4
Justin Ridgewell <justin@ridgewell.name>
2013-07-22 00:18:39 -0400

    Begin specing Minionette.View

--- spec/view_spec.js
+++ spec/view_spec.js
@@ -1,15 +1,102 @@
 define(function(require) {
     describe('Minionette.View', function() {
+        beforeEach(function() {
+            this.view = new Minionette.View;
+        })
+        afterEach(function() {
+            this.view.remove();
+        });
+
         it('Exists', function() {
             expect(Minionette.View).to.not.be.undefined;
         });
 
-        it("calls Backbone.View's constructor", function() {
-            var spy = Backbone.View = this.sinon.spy();
+        describe("constructor", function() {
+            it("calls Backbone.View's constructor", function() {
+                var spy = this.sinon.spy(Backbone, 'View');
 
-            new Minionette.View;
+                new Minionette.View;
 
-            spy.should.have.been.called;
-        })
+                expect(spy).to.have.been.called;
+            });
+
+            it("initializes _subViews", function() {
+                expect(this.view._subViews).to.not.be.undefined;
+            });
+
+            xit("listens for model events", function() {});
+            xit("listens for collection events", function() {});
+
+        });
+
+        describe("instances", function() {
+            it("creates #template function", function() {
+                expect(this.view.template).to.be.a.function;
+            });
+
+            describe("#delegateEvents", function() {
+                it("calls Backbone.View's #delegateEvents", function() {
+                    var spy = this.sinon.spy(Backbone.View.prototype, 'delegateEvents');
+
+                    this.view.delegateEvents();
+
+                    expect(spy).to.have.been.called;
+                });
+            });
+
+            describe("#remove", function() {
+                it("triggers remove:before event", function() {
+                    var spy = this.sinon.spy();
+                    this.view.on('remove:before', spy);
+
+                    this.view.remove();
+
+                    expect(spy).to.have.been.called;
+                });
+
+                it("triggers remove event", function() {
+                    var spy = this.sinon.spy();
+                    this.view.on('remove', spy);
+
+                    this.view.remove();
+
+                    expect(spy).to.have.been.called;
+                });
+
+                it("removes subViews", function() {
+                    var v = new Backbone.View,
+                        spy = this.sinon.spy(v, 'remove');
+                    this.view._subViews[0] = v;
+
+                    this.view.remove();
+
+                    expect(spy).to.have.been.called;
+                });
+            });
+
+            describe("#assign", function() {
+                xit();
+            });
+
+            describe("#_jqueryRemove", function() {
+                it("is called when jQuery removes $el", function() {
+                    var spy = this.sinon.spy(this.view, '_jqueryRemove');
+
+                    this.view.delegateEvents();
+                    this.view.$el.remove();
+
+                    expect(spy).to.have.been.called;
+                });
+
+                it("is not called with #undelegateEvents", function() {
+                    var spy = this.sinon.spy(this.view, '_jqueryRemove');
+
+                    this.view.delegateEvents();
+                    this.view.undelegateEvents();
+
+                    expect(spy).to.not.have.been.called;
+                });
+            });
+        });
     });
 });
