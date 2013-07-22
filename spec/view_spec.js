8dd2e864615b8a2616cc571a99ef42b15239b01e
Justin Ridgewell <jridgewell@cloudspace.com>
2013-07-22 13:14:17 -0400

    More Minionette.View testing.

--- spec/view_spec.js
+++ spec/view_spec.js
@@ -75,7 +75,66 @@ define(function(require) {
             });
 
             describe("#assign", function() {
-                xit();
+                beforeEach(function() {
+                    this.subView = new Backbone.View({id: 'subView'});
+                });
+
+                it("takes a selector and a subView as arguments", function() {
+                    var selector = '.selector',
+                        spy = this.sinon.spy(this.subView, 'setElement');
+                    this.view.$el.append('<div class="selector" />');
+
+                    this.view.assign(selector, this.subView);
+
+                    expect(spy).to.have.been.calledWith(this.view.$(selector));
+                });
+
+                it("takes an object (with keys as selectors and values as subViews) as an argument", function() {
+                    var subView2 = new Backbone.View,
+                        subViews = {
+                            '.selector': this.subView,
+                            '.test': subView2
+                        },
+                        spy = this.sinon.spy(this.subView, 'setElement'),
+                        spy2 = this.sinon.spy(subView2, 'setElement');
+                    this.view.$el.append('<div class="selector" />');
+                    this.view.$el.append('<div class="test" />');
+
+                    this.view.assign(subViews);
+
+                    expect(spy).to.have.been.calledWith(this.view.$('.selector'));
+                    expect(spy2).to.have.been.calledWith(this.view.$('.test'));
+                });
+
+                describe("replace param", function() {
+                    it("can optionally take a replace param", function() {
+                        var selector = '.selector',
+                        spy = this.sinon.spy(this.subView, 'setElement');
+                        this.view.$el.append('<div class="selector" />');
+
+                        this.view.assign(selector, this.subView, true);
+
+                        expect(spy).to.not.have.been.called;
+                        expect(this.view.$(this.subView.el).get(0)).to.not.be.undefined;
+                    });
+
+                    it("can optionally take a replace param with alternate syntax", function() {
+                        var subView2 = new Backbone.View,
+                        subViews = {
+                            '.selector': this.subView,
+                            '.test': subView2
+                        },
+                        spy = this.sinon.spy(this.subView, 'setElement'),
+                        spy2 = this.sinon.spy(subView2, 'setElement');
+                        this.view.$el.append('<div class="selector" />');
+                        this.view.$el.append('<div class="test" />');
+
+                        this.view.assign(subViews, true);
+
+                        expect(spy).to.not.have.been.calledWith(this.view.$('.selector'));
+                        expect(spy2).to.not.have.been.calledWith(this.view.$('.test'));
+                    });
+                });
             });
 
             describe("#_jqueryRemove", function() {
@@ -89,6 +148,11 @@ define(function(require) {
                 });
 
                 it("is not called with #undelegateEvents", function() {
+                    // Why are we testing this?
+                    // With the way jQuery defines special events,
+                    // the "remove" event will actually fire whenever we
+                    // remove the event handler from the DOM element, calling
+                    // the callback. We don't want that.
                     var spy = this.sinon.spy(this.view, '_jqueryRemove');
 
                     this.view.delegateEvents();
