54320f6b1d5c27fa0d53783f1be7f2aeffbd8a3f
Justin Ridgewell <jridgewell@cloudspace.com>
2013-07-23 11:03:24 -0400

    Listen to default Model events in ModelView

--- src/model_view.js
+++ src/model_view.js
@@ -1,4 +1,11 @@
 Minionette.ModelView = Minionette.View.extend({
+
+    // Listen to the default events
+    modelEvents: {
+        'change': 'render',
+        'destroy': 'remove'
+    },
+
     // The data that is sent into the template function.
     // Override this to provide custom data.
     serializeData: function() {
