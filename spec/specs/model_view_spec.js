4db531ca2b0a474c2c2ed750fe2433f2b3f69e58
Justin Ridgewell <jridgewell@cloudspace.com>
2013-07-23 14:31:57 -0400

    Update the specs to new View#render

--- spec/specs/model_view_spec.js
+++ spec/specs/model_view_spec.js
@@ -18,71 +18,8 @@ define(function() {
 
             describe("#serializeData", function() {
                 it("returns models attributes", function() {
-                    expect(this.view.serializeData()).to.equal(this.model.attributes);
-                });
-            });
-
-            describe("#render", function() {
-                it("fires 'render:before' event", function() {
-                    var spy = this.sinon.spy();
-                    this.view.on('render:before', spy);
-
-                    this.view.render();
-
-                    expect(spy).to.have.been.called;
-                });
-
-                it("fires 'render' event", function() {
-                    var spy = this.sinon.spy();
-                    this.view.on('render', spy);
-
-                    this.view.render();
-
-                    expect(spy).to.have.been.called;
-                });
-
-                it("detaches subViews before emptying $el", function() {
-                    var subView = new Minionette.View(),
-                        spy = this.sinon.spy();
-                    this.view.$el.append('<div id="test" />');
-                    this.view.assign('#test', subView);
-                    subView.$el.on('click', spy);
-
-                    this.view.render();
-
-                    expect(spy).to.not.have.been.called;
-                    subView.$el.trigger('click');
-                    expect(spy).to.have.been.called;
-                });
-
-                it("passes #serializeData's output to #template", function() {
-                    var spy = this.sinon.spy(this.view, 'template'),
-                    serializeData = _.uniqueId;
-                    this.view.serializeData = function() {
-                        return serializeData;
-                    };
-
-                    this.view.render();
-
-                    expect(spy).to.have.been.calledWith(serializeData);
-                });
-
-                it("passes #template's output to $el#html", function() {
-                    var spy = this.sinon.spy(this.view.$el, 'html'),
-                    template = _.uniqueId;
-                    this.view.template = function() {
-                        return template;
-                    };
-
-                    this.view.render();
-
-                    expect(spy).to.have.been.calledWith(template);
-                });
-
-                it("returns the view", function() {
-                    var ret = this.view.render();
-
-                    expect(ret).to.equal(this.view);
+                    this.model.set(_.uniqueId(), _.uniqueId());
+                    expect(this.view.serializeData()).to.deep.equal(this.model.attributes);
                 });
             });
         });
