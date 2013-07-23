22c94de6db36c433722f24cf960743e40ec3a8f9
Justin Ridgewell <jridgewell@cloudspace.com>
2013-07-23 17:21:40 -0400

    Finish CollectionView specs

--- spec/specs/collection_view_spec.js
+++ spec/specs/collection_view_spec.js
@@ -15,6 +15,10 @@ define(function() {
                     expect(this.view.collectionEvents.reset).to.equal('render');
                 });
 
+                it("#render on collection's 'sort' event", function() {
+                    expect(this.view.collectionEvents.sort).to.equal('render');
+                });
+
                 it("#removeOne on collection's 'remove' event", function() {
                     expect(this.view.collectionEvents.remove).to.equal('removeOne');
                 });
@@ -24,6 +28,155 @@ define(function() {
                 });
             });
 
+            describe("#render", function() {
+                it("triggers 'render:before' event", function() {
+                    var spy = this.sinon.spy();
+                    this.view.on('render:before', spy);
+
+                    this.view.render();
+
+                    expect(spy).to.have.been.called;
+                });
+
+                it("triggers 'render' event", function() {
+                    var spy = this.sinon.spy();
+                    this.view.on('render', spy);
+
+                    this.view.render();
+
+                    expect(spy).to.have.been.called;
+                });
+
+                it("appends a modelView for each model in the collection", function() {
+                    // TODO: Shouldn't know about #_addModelView
+                    var models = [
+                        new Backbone.Model(),
+                        new Backbone.Model(),
+                        new Backbone.Model()
+                    ];
+                    this.collection.add(models);
+                    var spy = this.sinon.spy(this.view, '_addModelView');
+
+                    this.view.render();
+
+                    expect(spy).to.be.calledThrice;
+                });
+            });
+
+            describe("#addOne", function() {
+                beforeEach(function() {
+                    this.model = new Backbone.Model();
+                });
+
+                it("triggers 'addOne:before' event", function() {
+                    var spy = this.sinon.spy();
+                    this.view.on('addOne:before', spy);
+
+                    this.view.addOne(this.model);
+
+                    expect(spy).to.have.been.called;
+                });
+
+                it("triggers 'addOne' event", function() {
+                    var spy = this.sinon.spy();
+                    this.view.on('addOne', spy);
+
+                    this.view.addOne(this.model);
+
+                    expect(spy).to.have.been.called;
+                });
+
+                it("creates a view from ModelView", function() {
+                    var spy = this.sinon.spy(Backbone, 'View'),
+                        view = new Minionette.CollectionView({collection: this.collection});
+
+                    view.addOne(this.model);
+
+                    expect(spy).to.have.been.called;
+                });
+
+                it("returns the new modelView", function() {
+                    var view = this.view.addOne(this.model);
+
+                    expect(view).to.be.instanceOf(Backbone.View);
+                });
+
+                it("add the new modelView to _subViews", function() {
+                    // TODO: Knows too much
+                    var view = this.view.addOne(this.model);
+
+                    expect(this.view._subViews[view.cid]).to.equal(view);
+                });
+
+                it("sets the new modelView's _parentView to this", function() {
+                    var view = this.view.addOne(this.model);
+
+                    expect(view._parentView).to.equal(this.view);
+                });
+
+                it("appends the new modelView's el to this.$el", function() {
+                    var view = this.view.addOne(this.model);
+
+                    expect(this.view.$(view.el)[0]).to.equal(view.el);
+                });
+            });
+
+            describe("#removeOne", function() {
+                beforeEach(function() {
+                    this.model = new Backbone.Model();
+                    this.modelView = this.view.addOne(this.model);
+                });
+
+                it("triggers 'removeOne:before' event", function() {
+                    var spy = this.sinon.spy();
+                    this.view.on('removeOne:before', spy);
+
+                    this.view.removeOne(this.model);
+
+                    expect(spy).to.have.been.called;
+                });
+
+                it("triggers 'removeOne' event", function() {
+                    var spy = this.sinon.spy();
+                    this.view.on('removeOne', spy);
+
+                    this.view.removeOne(this.model);
+
+                    expect(spy).to.have.been.called;
+                });
+
+                it("returns the removed view", function() {
+                    var ret = this.view.removeOne(this.model);
+
+                    expect(ret).to.equal(this.modelView);
+                });
+
+                it("calls #remove on the view", function() {
+                    var spy = this.sinon.spy(this.modelView, 'remove');
+
+                    this.view.removeOne(this.model);
+
+                    expect(spy).to.have.been.called;
+                });
+            });
+
+            describe("#_getModelView", function() {
+                beforeEach(function() {
+                    this.View = Minionette.CollectionView.extend({
+                        ModelView: 'DefaultModelView'
+                    });
+                });
+
+                it("returns this#ModelView", function() {
+                    var view = new this.View();
+                    expect(view._getModelView()).to.equal('DefaultModelView');
+                });
+
+                it("returns ModelView passed in at instantiation", function() {
+                    var view = new this.View({ModelView: 'instantiation'});
+                    expect(view._getModelView()).to.equal('instantiation');
+                });
+            });
         });
     });
 });
