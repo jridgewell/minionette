7bf279a3de6688607bd2b101961933d0b5007430
Justin Ridgewell <jridgewell@cloudspace.com>
2013-07-23 11:51:58 -0400

    Monkeypatch jQuery.cleanData to fire a 'remove' event

--- src/jquery.event.special.remove.js
+++ src/jquery.event.special.remove.js
@@ -1,8 +1,14 @@
 // Force jQuery to emit remove events.
 // Our views bind to it to see if they
 // have been removed and should be cleaned.
-$.event.special.remove = {
-    remove: function(e) {
-        if (e.handler) { e.handler(); }
-    }
-};
+if ($) {
+    var _cleanData = $.cleanData;
+    $.cleanData = function( elems ) {
+        for ( var i = 0, elem; (elem = elems[i]) !== undefined; i++ ) {
+            try {
+                $( elem ).triggerHandler( 'remove' );
+            } catch( e ) {}
+        }
+        _cleanData( elems );
+    };
+}
