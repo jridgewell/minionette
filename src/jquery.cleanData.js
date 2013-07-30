cca1cb2bbe9412e3e12f665f726c6a923f0c9f5c
Justin Ridgewell <justin@ridgewell.name>
2013-07-29 23:57:44 -0400

    Only wrap jQuery#cleanData when jQuery doesn't fire 'remove' events

--- src/jquery.cleanData.js
+++ src/jquery.cleanData.js
@@ -3,13 +3,26 @@
 // have been removed and should be cleaned.
 (function($) {
     if (!$) { return; }
-    var _cleanData = $.cleanData;
-    $.cleanData = function(elems) {
-        _.each(elems, function(elem) {
-            try {
-                $(elem).triggerHandler('remove');
-            } catch(e) {}
-        });
-        _cleanData(elems);
-    };
+    var _cleanData = $.cleanData,
+        testElement = $('<span />'),
+        shouldWrap = true;
+
+    // Check to see if jQuery already
+    // fires a 'remove' event. If it does,
+    // then don't wrap #cleanData.
+    testElement.on('remove', function() {
+        shouldWrap = false;
+    });
+    testElement.remove();
+
+    if (shouldWrap) {
+        $.cleanData = function(elems) {
+            _.each(elems, function(elem) {
+                try {
+                    $(elem).triggerHandler('remove');
+                } catch(e) {}
+            });
+            _cleanData(elems);
+        };
+    }
 })(jQuery);
