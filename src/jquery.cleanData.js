03f10e32397adeb8cdebaba0500bbca0064b5ce8
Justin Ridgewell <justin@ridgewell.name>
2013-07-31 23:16:59 -0400

    Unwrap jQuery#cleanData wrapper

--- src/jquery.cleanData.js
+++ src/jquery.cleanData.js
@@ -1,28 +1,24 @@
 // Force jQuery to emit remove events.
 // Our views bind to it to see if they
 // have been removed and should be cleaned.
-(function($) {
-    if (!$) { return; }
-    var _cleanData = $.cleanData,
-        testElement = $('<span />'),
-        shouldWrap = true;
+var _cleanData = $.cleanData,
+    testElement = $('<span />');
 
-    // Check to see if jQuery already
-    // fires a 'remove' event. If it does,
-    // then don't wrap #cleanData.
-    testElement.on('remove', function() {
-        shouldWrap = false;
-    });
-    testElement.remove();
+// Check to see if jQuery already
+// fires a 'remove' event. If it does,
+// then don't wrap #cleanData.
+testElement.on('remove', function() {
+    _cleanData = null;
+});
+testElement.remove();
 
-    if (shouldWrap) {
-        $.cleanData = function(elems) {
-            _.each(elems, function(elem) {
-                try {
-                    $(elem).triggerHandler('remove');
-                } catch(e) {}
-            });
-            _cleanData(elems);
-        };
-    }
-})(jQuery);
+if (_cleanData) {
+    $.cleanData = function(elems) {
+        _.each(elems, function(elem) {
+            try {
+                $(elem).triggerHandler('remove');
+            } catch(e) {}
+        });
+        _cleanData(elems);
+    };
+}
