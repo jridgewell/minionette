9cb53033ba4649852c24fc96a9dc79bd757f4995
Justin Ridgewell <justin@ridgewell.name>
2013-12-12 20:40:26 -0500

    Rename spec directory to test.

--- Gruntfile.js
+++ Gruntfile.js
@@ -55,7 +55,7 @@ module.exports = function(grunt) {
         },
 
         mocha: {
-            browser: ['spec/index.html'],
+            browser: ['test/index.html'],
             options: {
                 // run: true
             }
