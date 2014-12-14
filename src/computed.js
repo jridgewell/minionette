6a6c33b3512d63df7b8ff7b812eba13ba4564f1d
Justin Ridgewell <justin@ridgewell.name>
2014-12-14 14:20:40 -0500

    Comment Computed

--- src/computed.js
+++ src/computed.js
@@ -1,3 +1,7 @@
+// A helper function that specifies a function's dependent model attributes.
+// When used in conjunction with Minionette.Model, any time a dependency
+// is updated, the computing function is called. The corresponding model
+// attribute will be updated with the returned value.
 Minionette.Computed = function() {
     var dependencies = _.initial(arguments);
     var fn = _.last(arguments);
