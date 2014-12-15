a6ddbc4befe0e2a8fe5b8e09b964c42e09edcf99
Justin Ridgewell <justin@ridgewell.name>
2014-12-14 19:03:41 -0500

    Rename attempt's variables

--- src/attempt.js
+++ src/attempt.js
@@ -1,15 +1,15 @@
 // A helper function, similar to _.result
-// that will call the property on obj, unless obj
+// that will call the prop on obj, unless obj
 // is undefined or null. Passes the 3rd param
 // as arguments to the method.
-function attempt(obj, property, args) {
+function attempt(obj, prop, args) {
     // Return undefined unless obj
     // is not null or undefined
     if (obj == null) { return void 0; }
-    var prop = obj[property];
+    var fn = obj[prop];
 
-    if (_.isFunction(prop)) {
+    if (_.isFunction(fn)) {
         var apply = _.isArray(args);
-        return apply ? prop.apply(obj, args) : obj[property](args);
+        return apply ? fn.apply(obj, args) : obj[prop](args);
     }
 }
