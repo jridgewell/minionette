1b3905f204dd859086566176e482e8c0b30e489c
Justin Ridgewell <justin@ridgewell.name>
2014-12-18 14:48:26 -0500

    UMD-ify
    Closes #3, for now. Still looking into ES6 modules.

--- src/build/minionette.js
+++ src/build/minionette.js
@@ -1,14 +1,26 @@
-'use strict';
-var Minionette = (function(_, Backbone) {
+(function(root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define(['underscore', 'backbone'], factory);
+  } else if (typeof exports !== 'undefined') {
+    module.exports = factory(require('underscore'), require('backbone'));
+  } else {
+    root.Minionette = factory(root._, root.Backbone);
+  }
+})(this, function(_, Backbone) {
+    'use strict';
 
     // Define and export the Minionette namespace
     var Minionette = Backbone.Minionette = {};
 
-// @include .tmp/attempt.js
-// @include .tmp/region.js
-// @include .tmp/view.js
-// @include .tmp/model_view.js
-// @include .tmp/collection_view.js
+    // @include ../attempt.js
+    // @include ../model.js
+    // @include ../region.js
+    // @include ../view.js
+    // @include ../model_view.js
+    // @include ../collection_view.js
+    // @include ../router.js
+    // @include ../computed.js
+    // @include ../trigger.js
 
     return Minionette;
-})(_, Backbone);
+});
