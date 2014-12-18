e606ea8e367a1e1883f260ee56d6e9d884a3edca
Justin Ridgewell <justin@ridgewell.name>
2014-12-18 11:37:29 -0500

    Spec trigger() as a function

--- test/specs/trigger_spec.js
+++ test/specs/trigger_spec.js
@@ -6,6 +6,72 @@ describe("Minionette.trigger()", function() {
         });
     });
 
+    describe("when used as a function", function() {
+        var obj;
+        beforeEach(function() {
+            obj = _.extend({}, Backbone.Events);
+        });
+
+        it("calls 'onEvent' method", function() {
+            var spy = obj.onEvent = sinon.spy();
+
+            Minionette.trigger.call(obj, 'event');
+
+            expect(spy).to.have.been.called;
+        });
+
+        it("passes event's arguments as params", function() {
+            var spy = obj.onEvent = sinon.spy();
+
+            Minionette.trigger.call(obj, 'event', 1, 2, 3);
+
+            expect(spy).to.have.been.calledWith(1, 2, 3);
+        });
+
+        it("triggers event handlers as normal", function() {
+            var spy = sinon.spy();
+            obj.on('event', spy);
+
+            Minionette.trigger.call(obj, 'event', 1, 2, 3);
+
+            expect(spy).to.have.been.calledWith(1, 2, 3);
+        });
+
+        describe("when passed multiple space delimited events", function() {
+            it("calls each on method", function() {
+                var spy = obj.onEvent = sinon.spy();
+                var spy2 = obj.onEvent2 = sinon.spy();
+
+                Minionette.trigger.call(obj, 'event event2');
+
+                expect(spy).to.have.been.called;
+                expect(spy2).to.have.been.called;
+            });
+
+            it("passes event's arguments as params", function() {
+                var spy = obj.onEvent = sinon.spy();
+                var spy2 = obj.onEvent2 = sinon.spy();
+
+                Minionette.trigger.call(obj, 'event event2', 1, 2, 3);
+
+                expect(spy).to.have.been.calledWith(1, 2, 3);
+                expect(spy2).to.have.been.calledWith(1, 2, 3);
+            });
+
+            it("triggers event handlers as normal", function() {
+                var spy = sinon.spy();
+                var spy2 = sinon.spy();
+                obj.on('event', spy);
+                obj.on('event2', spy2);
+
+                Minionette.trigger.call(obj, 'event event2', 1, 2, 3);
+
+                expect(spy).to.have.been.calledWith(1, 2, 3);
+                expect(spy2).to.have.been.calledWith(1, 2, 3);
+            });
+        });
+    });
+
     describe("when mixed in", function() {
         var obj;
         beforeEach(function() {
