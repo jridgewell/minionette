82af323023679e4154bcd7e396471c2c58ec95d1
Justin Ridgewell <justin@ridgewell.name>
2014-12-18 00:43:00 -0500

    Add event splitter support.

--- src/trigger.js
+++ src/trigger.js
@@ -1,9 +1,19 @@
 Minionette.trigger = function trigger(event) {
+    var eventSplitter = /\s+/;
+    var args = _.tail(arguments);
+
+    if (eventSplitter.test(event)) {
+        _.each(event.split(eventSplitter), function(event) {
+            trigger.apply(this, [event].concat(args));
+        }, this);
+        return this;
+    }
+
     var method = 'on' + event.replace(/(?:^|:)(\w)/, function(_match, letter) {
         return letter.toUpperCase();
     });
 
-    attempt(this, method, _.tail(arguments));
+    attempt(this, method, args);
 
     return trigger._trigger.apply(this, arguments);
 };
