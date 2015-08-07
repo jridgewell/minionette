7d942017949c33d14031c6cdf323c7d1c3b2fb5d
Justin Ridgewell <justin@ridgewell.name>
2015-08-07 18:58:13 -0400

    Drop Model and Computed

--- src/minionette.js
+++ src/minionette.js
@@ -1,6 +1,4 @@
 import CollectionView from './collection_view';
-import Computed from './computed';
-import Model from './model';
 import ModelView from './model_view';
 import Region from './region';
 import Router from './router';
@@ -9,8 +7,6 @@ import View from './view';
 
 export default {
   CollectionView: CollectionView,
-  Computed: Computed,
-  Model: Model,
   ModelView: ModelView,
   Region: Region,
   trigger: trigger,
